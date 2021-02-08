66627940c89193e29e1f850b442300eb59d8f237
Mike Morris <eight20eight@gmail.com>
2021-02-08 15:35:10 -0500

    ask name and greet

--- public/main.js
+++ public/main.js
@@ -1,7 +1,27 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+function main() 
+{
+
+     const numberOfCupsOfCoffee = 0;
+     const fullName = "Mike Morris";
+     console.log (`I am ${fullName}, I drink ${numberOfCupsOfCoffee} cups of coffee a day.`);
+
+     const aboutMe ={
+        myFavoriteMovies: ['Jaws', 'One', 'The Thing'],
+        fullName: 'Mike Morris',
+        luckyNumber: 9
+      
+     }
+     const userName = prompt("Please enter your name");
+     console.log (`Hi ${userName} Welcome to my program`);
+
+
+
+
+     
+  
+
+
+  
 }
 
 document.addEventListener('DOMContentLoaded', main)
